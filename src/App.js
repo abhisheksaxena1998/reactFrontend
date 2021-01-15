807d6cd2a4a4e5b69aa7d489c97eddcf8e221e57
Abhishek Saxena <abhi123923@gmail.com>
2021-01-15 17:14:53 +0530

    pagination done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
+import { MDBDataTable } from 'mdbreact';
 
 import React from 'react';
 
@@ -17,15 +18,63 @@ class myComponent extends React.Component {
     fetch(apiUrl)
       .then((response) => response.json())
       .then(data => {
-        this.setState({ records: data , isLoading: false})});
+        this.setState({ records: data , isLoading: false})
+        console.log(this.state)
+      });
   }
   render() 
   {
+    const data = {
+      columns: [
+        {
+          label: 'Unique Id',
+          field: 'unique_id',
+         
+          width: 150
+        },
+        {
+          label: 'Index',
+          field: 'index',
+          
+          width: 270
+        },
+        {
+          label: 'Name',
+          field: 'name',
+         
+          width: 200
+        },
+        {
+          label: 'Result',
+          field: 'result',
+       
+          width: 150
+        },
+        {
+          label: 'Time',
+          field: 'time',
+         
+          width: 270
+        },
+        {
+          label: 'Phone Number',
+          field: 'phone number',
+          width: 200
+        },
+        {
+          label: 'Created At',
+          field: 'created_at',
+          width: 200
+        }
+      ],
+      rows: this.state.records
+      }
     const { records, isLoading } = this.state;
     if (isLoading) {
       return <p>Loading ...</p>;
     }
     return (
+      <div>
       <ul>
         {records.map(hit =>
           <li key={hit.unique_id}>
@@ -34,6 +83,13 @@ class myComponent extends React.Component {
           </li>
         )}
       </ul>
+      <MDBDataTable
+      striped
+      bordered
+      small
+      data={data}
+    />
+    </div>
     );
   }
 }
